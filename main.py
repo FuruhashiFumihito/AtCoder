cd61a19e792bd88a3936a2e168f140dc9507cd0d
furuhashi <fumifuruhashi@gmail.com>
2023-06-04 01:20:50 +0900

    add: acc

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import math
+import os
+import sys
+import re
+import collections
+import itertools
+import functools
+import operator
+import bisect
+import heapq
+import copy
+import time
+import random
+
+n = int(input())
+l = list(map(int, input().split()))
